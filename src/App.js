07720633ec2cf6b5167fb50e95d0a824792644a5
bipin-shrestha <iambpn.shrestha@gmail.com>
2020-12-13 16:33:08 -0800

    Routers created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
-import Header from "./components/Header";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route} 
+from 'react-router-dom';
+import Confirmation from "./components/Confirmation";
+import Form from "./components/Form";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-  <Header/>
+    <Router>
+      <Switch>
+        <Route exact path='/' component ={Home}/>
+        <Route exact path='/pizza' component ={Form}/>
+        <Route exact path='/confirmation' component ={Confirmation}/>
+      </Switch>
+    </Router>
+ 
   );
 };
 export default App;
---
+++ src/components/Confirmation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Confirmation(){
+
+    return(
+        <h1>Pizza Confirmation Loaded</h1>
+    )
+
+}
