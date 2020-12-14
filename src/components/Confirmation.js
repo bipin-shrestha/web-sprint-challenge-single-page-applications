b178747a5af848d60832be04a63d770720687932
bipin-shrestha <iambpn.shrestha@gmail.com>
2020-12-13 20:49:30 -0800

    MVP completed

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Header from './Header';
 
-export default function Confirmation(){
-
+export default function Confirmation(props){
+    const data = props.location.state.data;
     return(
-        <h1>Pizza Confirmation Loaded</h1>
+    <div>
+        <Header/>
+        Customer Name: {data.customername}<br/>
+        Pizza Size: {data.pizzaSize}<br/>
+        Toppings:
+        <br/>
+        {data.pepperoni?"Pepperoni,":""}{data.onions?"Onions,":""}{data.pineapple?"Pineapple,":""}{data.sausage?"Sausage":""}
+        <br/>
+        Special Instruction: {data.specialInstruction}
+    </div>       
     )
 
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,89 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import Header from './Header';
-
+import * as yup from 'yup';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
 
 export default function Form(){
- 
+const [ formState, setformState ]= useState({
+    customername: "",
+    pizzaSize: "small",
+    pepperoni: false,
+    sausage: false,
+    onions: false,
+    pineapple: false,
+    specialInstruction: ""
+}); 
+const [ errors, setErrors ]= useState({ 
+    customername: "",
+    pizzaSize: "",
+    pepperoni: "",
+    sausage: "",
+    onions: "",
+    pineapple: "",
+    specialInstruction: ""
+});
+const [buttonDisabled, setButtonIsDisabled] = useState(true);
+const formSchema = yup.object().shape({
+    customername: yup.string().required("Cannot be empty").min(2,"Must be atleast 2 characters"),
+    pizzaSize: yup.string(),
+    pepperoni: yup.boolean(),
+    sausage: yup.boolean(),
+    onions: yup.boolean(),
+    pineapple: yup.boolean(),
+    specialInstruction: yup.string()
+    
+});
 
+useEffect(()=>{
+    formSchema.isValid(formState).then((valid)=>{
+        console.log("is the form valid???", valid);
+        setButtonIsDisabled(!valid);
+    });
+},[formState]);
+
+const validate =(event) =>{
+    yup
+    .reach(formSchema, event.target.name)
+    .validate(event.target.type === "checkbox" ? event.target.checked : event.target.value)
+    .then((validate)=>{
+        setErrors({ ...errors, [event.target.name]:""});
+    })
+    .catch((error)=>{
+        setErrors({...errors,[event.target.name]:error.errors[0]});
+    });
+};
+const inputChange = (event) => {
+    event.persist();
+    const newFormState = {
+        ...formState,[event.target.name]:  
+        event.target.type === 'checkbox' ? event.target.checked  : event.target.value
+    };
+    validate(event);
+    console.log(errors);
+    setformState(newFormState);
+};
+const history = useHistory();
+const formSubmit = (event) => {
+    event.preventDefault();
+    //console.log(formState);
+    axios
+    .post("https://reqres.in/api/users", formState)
+    .then((response) =>{
+        console.log(response.data);
+        history.push({
+            pathname: "/confirmation",
+            state: {data: response.data}
+        });
+    });
+
+   
+}
 
     return(
         <div>
             <Header/>
-            <form>
+            <form onSubmit={formSubmit}>
                 <label htmlFor="customername">
                     Customer Name :
                     <br/>
@@ -17,17 +91,24 @@ export default function Form(){
                     id='customername'
                     type='text'
                     name='customername'
+                    value={formState.name}
+                    onChange={inputChange}
                     />
+                    {errors.customername.length > 0 ? <p>{errors.customername}</p>:null }
                 </label>
                 <br/>
                 <label htmlFor="pizzaSize">
                     Pizza Size :  
                     <select
                         id='pizzaSize'
-                        name='pizzaSize'>
+                        name='pizzaSize'
+                        value={formState.pizzaSize}
+                        onChange={inputChange}  
+                        > 
                          <option value='small'>Small</option>
                          <option value='medium'>Medium</option>
                          <option value='large'>Large</option>
+                         
                     </select>
                      <br/>
                 </label>
@@ -37,6 +118,8 @@ export default function Form(){
                         type='checkbox'
                         id='pepperoni'
                         name='pepperoni'
+                        checked={formState.pepperoni}
+                        onChange={inputChange}
                     />
                 </label>
                 <br/>
@@ -46,6 +129,8 @@ export default function Form(){
                         type='checkbox'
                         id='sausage'
                         name='sausage'
+                        checked={formState.sausage}
+                        onChange={inputChange}
                     />
                 </label>
                 <br/>
@@ -55,6 +140,8 @@ export default function Form(){
                         type='checkbox'
                         id='onions'
                         name='onions'
+                        checked={formState.onions}
+                        onChange={inputChange}
                     />
                 </label>
                 <br/>
@@ -64,6 +151,8 @@ export default function Form(){
                         type='checkbox'
                         id='pineapple'
                         name='pineapple'
+                        checked={formState.pineapple}
+                        onChange={inputChange}
                     />
                 </label>
                 <br/>
@@ -73,11 +162,13 @@ export default function Form(){
                 <textarea
                     id='specialInstruction'
                     name='specialInstruction'
+                    value={formState.specialInstruction}
+                    onChange={inputChange}
                     />
                 </label>
                 <br/>
-                <button type='submit'>Place the Order</button>
-
+                <button type='submit' disabled={buttonDisabled}>Place the Order</button>
+              
             </form>
 
         </div>
